1af8128c4162b0efde062bc5563490c2230e15e4
lmYourGod <carlos2003cjj@gmail.com>
2022-11-28 05:52:42 +0100

    Update TOCHO

--- SantMarcWeb/Assets/Members/Carlos/Scripts/Vehicles/Plane/PlaneSound.cs
+++ SantMarcWeb/Assets/Members/Carlos/Scripts/Vehicles/Plane/PlaneSound.cs
@@ -5,5 +5,22 @@ using UnityEngine;
 
 public class PlaneSound : MonoBehaviour
 {
-    
+    //Variables
+    [SerializeField] private AudioSource planeSound;
+
+    private void Start()
+    {
+        InvokeRepeating("PlaneSounds", 0 ,0.1f);   
+    }
+
+    private void PlaneSounds()
+    {
+        if (planeSound != null)
+        {
+            if (!planeSound.isPlaying)
+            {
+                planeSound.Play();   
+            }
+        }
+    }
 }
